39f7a9ed8ffe874c81c600d19aa8fff73ed46852
linlong <839450636@qq.com>
2019-11-22 15:58:19 +0800

    init nacos server

---
+++ src/main/java/com/learn/cloud/cloudnacos/CloudNacosApplication.java
@@ -0,0 +1,14 @@
+package com.learn.cloud.cloudnacos;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudNacosApplication.class, args);
+    }
+
+}
